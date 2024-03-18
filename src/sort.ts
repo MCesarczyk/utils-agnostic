3e2c6369d65c28e84538f57effde7e40e88f9d54
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-18 16:12:00 +0100

    feat(sort): simplify name, update sorting method to ascending by default

--- src/sort.ts
+++ src/sort.ts
@@ -1,12 +1,12 @@
-export const sortArrayOfObjectsDescending = <T>(array: Array<T>, field: keyof T) => {
+export const sortArrayOfObjects = <T>(array: Array<T>, field: keyof T) => {
   return array.sort((a, b) => {
     const stateA = String(a[field]).toLowerCase();
     const stateB = String(b[field]).toLowerCase();
     if (stateA < stateB) {
-      return 1;
+      return -1;
     }
     if (stateA > stateB) {
-      return -1;
+      return +1;
     }
     return 0;
   });
